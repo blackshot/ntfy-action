dae134f8d951ec3cdc4f0d625489e5d6be2e5ead
Hector Echiburu <hechiburu@prosys.cl>
2023-09-19 04:03:44 -0300

    now doesn't depends on context.payload

--- src/index.js
+++ src/index.js
@@ -1,156 +1,18 @@
 const core = require('@actions/core');
 const axios = require('axios');
 const github = require('@actions/github');
-//const FormData = require('form-data');
-//const fs = require('fs');
-
-async function githubmessage() {
-    const context = github.context;
-    const payload = context.payload;
-    let action_buttons
-    let message
-
-    switch (context.eventName) {
-        case 'push':
-            action_buttons = [{
-                    "action": "view",
-                    "label": "Compare",
-                    "url": payload.compare,
-                    "clear": true
-                },
-                {
-                    "action": "view",
-                    "label": "View Commit",
-                    "url": payload.head_commit.url,
-                    "clear": true
-                },
-                {
-                    "action": "view",
-                    "label": "Visit Repository",
-                    "url": payload.repository.html_url,
-                    "clear": true
-                }
-            ]
-
-            message = `${payload.head_commit.committer.name} has pushed ${context.sha.slice(-7)} to ${payload.repository.full_name}.\n\n` +
-                `Author: ${payload.head_commit.author.username}\n` +
-                `Committer: ${payload.head_commit.committer.name}\n` +
-                `Ref: ${context.ref}\n` +
-                `Pushed by: ${payload.pusher.name}\n` +
-                `Workflow Job Name: ${context.job}\n` +
-                `Workflow Name: ${context.workflow}\n\n` +
-                `Commit Message\n${payload.head_commit.message}`;
-
-            return [action_buttons, message]
-
-        case 'release':
-            action_buttons = [{
-                    "action": "view",
-                    "label": "Release URL",
-                    "url": payload.release.html_url,
-                    "clear": true
-                },
-                {
-                    "action": "view",
-                    "label": "Download Tar",
-                    "url": payload.release.tarball_url,
-                    "clear": true
-                },
-                {
-                    "action": "view",
-                    "label": "Download Zip",
-                    "url": payload.release.zipball_url,
-                    "clear": true
-                }
-            ]
-
-            message = `${payload.release.author.login} has ${payload.action} ${payload.release.tag_name} on ${payload.repository.full_name}.\n\n` +
-                `Repo: ${payload.repository.html_url}\n` +
-                `Name: ${payload.release.name}\n` +
-                `Author: ${payload.release.author.login}\n` +
-                `Prerelease: ${payload.release.prerelease}\n` +
-                `Workflow Job Name: ${context.job}\n` +
-                `Workflow Name: ${context.workflow}\n\n` +
-                `Release Message\n${payload.release.body}`;
-
-            return [action_buttons, message]
-
-        case 'schedule':
-            action_buttons = [{
-                    "action": "view",
-                    "label": "Visit Repository",
-                    "url": `https://github.com/${process.env.GITHUB_REPOSITORY}`,
-                    "clear": true
-                },
-                {
-                    "action": "view",
-                    "label": "View Run",
-                    "url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-                    "clear": true
-                }
-            ]
-
-            message = `Scheduled task '${context.job}' ran in ${process.env.GITHUB_REPOSITORY}.\n\n` +
-                `Workflow Name: ${context.workflow}\n` +
-                `Cron: ${context.payload.schedule}`;
-
-            return [action_buttons, message]
-
-        default:
-            action_buttons = [{
-                    "action": "view",
-                    "label": "Visit Repo",
-                    "url": payload.repository.html_url,
-                    "clear": true
-                },
-                {
-                    "action": "view",
-                    "label": "View Run",
-                    "url": `${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-                    "clear": true
-                }
-            ]
-
-            message = `Workflow '${context.workflow}' ran in ${payload.repository.full_name}\n\n` +
-                `Repository: ${payload.repository.full_name}\n` +
-                `Workflow Job Name: ${context.job}\n` +
-                `Event Name: ${context.eventName}`
-
-            return [action_buttons, message]
-    }
-}
 
 async function run() {
     try {
         const url = core.getInput('url')
         const headers = JSON.parse(core.getInput('headers') || '{}')
+        const actions = JSON.parse(core.getInput('actions') || '{}')
         const tags = core.getInput('tags').split(',');
         const topic = core.getInput('topic')
         const title = core.getInput('title') || 'GitHub Actions'
-        const details = core.getInput('details')
         const priority = core.getInput('priority') || 3
-        //const data = JSON.parse(core.getInput('data') || '{}')
-        //const name = core.getInput('name')
-        //const file = core.getInput('file')
-
-        //console.log(`URL: ${url}`)
-        //console.log(`Headers: ${headers}`)
-        //console.log(`Data: ${data}`)
-        //console.log(`File Name: ${name}`)
-        //console.log(`File Path: ${file}`)
-
-        //const form = new FormData();
-        //for (const [key, value] of Object.entries(data)) {
-        //  form.append(key, value)
-        //}
-
-        //if (name && file) {
-        //  form.append(name, fs.createReadStream(file));
-        //}
 
         core.info(`Connecting to endpoint (${url}) ...`)
-
-        let message = await githubmessage();
 
         const response = await axios({
             method: 'POST',
@@ -165,8 +27,8 @@ async function run() {
                 'topic': topic,
                 'tags': tags,
                 'title': (title),
-                'actions': message[0],
-                "message": message[1] + "\n\n" + details
+                'actions': actions,
+                "message": core.getInput('details')
             })
 
         })
